8e95033c29d25d20df02f3c6b29e453364821815
dami0806 <dami0806>
2022-12-28 21:19:22 +0900

    json형태 URL을 넣어서 get방식으로 정보 decoding

---
+++ NetworkingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingTest
+//
+//  Created by 박다미 on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
